987fb8635d9bfa7f3e1178ccdaa76c180ef9a976
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-25 18:10:46 +1000

    revising bubble sort

--- src/main/sort/BubbleSort.java
+++ src/main/sort/BubbleSort.java
@@ -4,22 +4,19 @@ public class BubbleSort {
     public static void main(String[] args) {
         // one of the many algorithm implementation
 
-        // implementation sorts array from right to left - in ascending order
-        // and there are other implementations for descending, sorting from
-        // left to right and etc.
+        // implementation sorts array from right to left - in ascending order and there are other implementations for
+        // descending, sorting from left to right and etc.
         int [] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-        // sorts from right to left
-        // for each iteration of outer loop - want to traverse and bubble
-        // largest element to the right and sort it
+        // sorts from right to left for each iteration of outer loop - want to traverse and bubble largest element to
+        // the right and sort it
 
-        // looking at code, can see that it is lesser than O(n^2), inner loop
-        // doesn't cross into sorted partition. However, when it comes to
-        // determining time complexity - we want a general idea of how the
-        // steps grow when the number of element grows.
+        // looking at code, can see that it is lesser than O(n^2), inner loop doesn't cross into sorted partition.
+        // However, when it comes to determining time complexity - we want a general idea of how the steps grow when
+        // the number of element grows.
 
-        // this code is optimized. Bubble sort would cross over into sorted
-        // parts of the array but for this code, it doesn't.
+        // this code is optimized. Bubble sort would cross over into sorted parts of the array but for this code, it
+        // doesn't.
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
             // i does not need to be larger than lastUnsortedIndex since that
             // partitioned part is already sorted
