52b1ba531bfd89a6e4813221ce5c795d1c59f064
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-26 09:10:54 +1000

    selection sorting and new file for insertion

--- src/main/sort/SelectionSort.java
+++ src/main/sort/SelectionSort.java
@@ -1,18 +1,30 @@
 package main.sort;
 
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-    int [] intArray = {20, 35, -15, 7, 55, 1, -22};
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+             int largest = 0;
 
-    
+             for (int i = 1; i <= lastUnsortedIndex; i++) {
+                 if (intArray[i] > intArray[largest]) {
+                     largest = i;
+                 }
+
+                 swap(intArray, largest, lastUnsortedIndex);
+             }
+        }
+    }
+
     /**
      * Swaps elements when it meets requirements
      *
      * @param array that will be passed to swap elements in it
-     * @param i index (initial) element that will be compared to the next
-     *          element
-     * @param j index (next) element that will be compared to the
-     *          initial element
+     * @param i     index (initial) element that will be compared to the next
+     *              element
+     * @param j     index (next) element that will be compared to the
+     *              initial element
      */
     public static void swap(int[] array, int i, int j) {
         if (i == j) {
