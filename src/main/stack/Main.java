4171b8b4b79786d4a39d20be5775bb4a27b23bb6
25Ericcheong <ericcheongkaikit@gmail.com>
2022-02-03 16:46:50 +1000

    implementing stack on your own

---
+++ src/main/stack/Main.java
@@ -0,0 +1,20 @@
+package main.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(10);
+
+        stack.push(new Employee("Jane", "Jones", 123));
+        stack.push(new Employee("John", "Doe", 456));
+        stack.push(new Employee("Mary", "Smith", 789));
+        stack.push(new Employee("Mike", "Wilson", 1011122));
+        stack.push(new Employee("Bill", "End", 8888));
+
+        stack.printStack();
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+    }
+}
