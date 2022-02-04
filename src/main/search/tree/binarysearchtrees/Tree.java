885594cda4827a47c85b54019602df828f3c0c84
25Ericcheong <ericcheongkaikit@gmail.com>
2022-02-05 07:43:09 +1000

    deleting a node when it only has 1 or 0 children node

--- src/main/search/tree/binarysearchtrees/Tree.java
+++ src/main/search/tree/binarysearchtrees/Tree.java
@@ -20,6 +20,36 @@ public class Tree {
         return null;
     }
 
+    public void delete(int value) {
+        root = delete(root, value);
+    }
+
+    // returns replacement node
+    private TreeNode delete(TreeNode subtreeRoot, int value) {
+        if (subtreeRoot == null) {
+            return subtreeRoot;
+        }
+
+        if (value < subtreeRoot.getData()) {
+            subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(), value));
+        } else if (value > subtreeRoot.getData()) {
+            subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
+        } else {
+            // in this case,value is equal to node currently focussing on
+            // if left child does not exist but right child does, it will replace current root node
+            // case 1 and 2: node to delete has 0 or 1 children
+            if (subtreeRoot.getLeftChild() == null) {
+                return subtreeRoot.getRightChild();
+            // right child does not exist
+            } else if (subtreeRoot.getRightChild() == null) {
+                return subtreeRoot.getLeftChild();
+            }
+        }
+
+        // reaching here means that it is not the node that we want to delete
+        return subtreeRoot;
+    }
+
     public int min() {
         if (root == null) {
             return Integer.MIN_VALUE;
