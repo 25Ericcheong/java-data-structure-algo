41ab1ea8ed3497fce389c804197492ff90e20ac9
25Ericcheong <ericcheongkaikit@gmail.com>
2022-02-06 09:01:09 +1000

    deletion implementation which heapifies sturcture if needed

--- src/main/search/heap/Heap.java
+++ src/main/search/heap/Heap.java
@@ -20,6 +20,29 @@ public class Heap {
         size++;
     }
 
+    public int delete(int index) {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+
+        // if node we want to delete is a root or if replacement value is smaller than parent node - in these both
+        // cases - we will only need to check nodes below them and try to fix and heapify strucutre
+        if (index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        } else {
+            fixHeapAbove(index);
+        }
+
+        size--;
+
+        return deletedValue;
+    }
+
     private void fixHeapAbove(int index) {
         int newValue = heap[index];
         while (index > 0 && newValue > heap[getParent(index)]) {
@@ -30,12 +53,70 @@ public class Heap {
         heap[index] = newValue;
     }
 
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        int childToSwap;
+
+        while (index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+
+            // check to ensure that we have index values that are within array
+            // done by checking last index of array in heap
+            // left child exist
+
+            // this is done first because heap must be complete - it must have a left child and right child or if not
+            // only a left child - which is why we start with checking left child
+            if (leftChild <= lastHeapIndex) {
+
+                // right child does not exist
+                if (rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                // right child exists, so we take larger of the
+                } else {
+                    childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+                }
+
+                // if this is false, we have heapify structure, so just break
+                if (heap[index] < heap[childToSwap]) {
+                    int tmp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = tmp;
+                } else {
+                    break;
+                }
+
+                // now within while loop, need to check if swapped element needs to be swapped with children below it
+                // again
+                index = childToSwap;
+
+            } else {
+                break;
+            }
+        }
+    }
+
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(heap[i]);
+            System.out.println(", ");
+        }
+        System.out.println();
+    }
+
     public boolean isFull() {
         return size == heap.length;
     }
 
     public int getParent(int index) {
         return (index - 1) / 2;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
     }
 
 }
